a680c195e2747e30f1eb23b54c7a52058a161bd2
Hampus Svärd <187299286+HampusSvard@users.noreply.github.com>
2025-04-02 18:18:04 +0200

    fortsättning resolutions o display modes

--- Scripts/Game/View/MainOptions.cs
+++ Scripts/Game/View/MainOptions.cs
@@ -1,11 +1,14 @@
 using Godot;
+using Goodot15.Scripts.Game.Controller;
 
 namespace Goodot15.Scripts.Game.View;
 
 public partial class MainOptions : Control {
 	private MenuController menuController;
+	private ResolutionManager resolutionManager;
 	
 	private OptionButton resolutionButton => GetNode<OptionButton>("ButtonContainer/ResolutionButton");
+	private OptionButton displayModeButton => GetNode<OptionButton>("ButtonContainer/DisplayModeButton");
 	private Button goBackButton => GetNode<Button>("GoBackButton");
 	
 	private readonly Vector2I[] resolutions = {
@@ -14,27 +17,46 @@ public partial class MainOptions : Control {
 		new Vector2I(1280, 720)
 	};
 
+	private readonly string[] displayModes = {
+		"WINDOWED",
+		"FULLSCREEN",
+		"BORDERLESS WINDOWED"
+	};
+
 	public override void _Ready() {
 		menuController = GetNode<MenuController>("/root/MenuController");
+		resolutionManager = GetNode<ResolutionManager>("/root/ResolutionManager");
+		
 		resolutionButton.Connect("item_selected", new Callable(this, nameof(OnResolutionSelected)));
+		displayModeButton.Connect("item_selected", new Callable(this, nameof(OnDisplayModeSelected)));
 		goBackButton.Pressed += OnBackButtonPressed;
 		
 		PopulateResolutionOptions();
+		PopulateDisplayModeOptions();
 	}
 	
 	private void PopulateResolutionOptions() {
 		resolutionButton.Clear();
 		
 		foreach (Vector2I res in resolutions) {
-			resolutionButton.AddItem($"        {res.X}x{res.Y}");
+			resolutionButton.AddItem($"         {res.X}x{res.Y}");
+		}
+	}
+
+	private void PopulateDisplayModeOptions() {
+		displayModeButton.Clear();
+
+		foreach (string displayMode in displayModes) {
+			displayModeButton.AddItem(displayMode);
 		}
 	}
 
 	private void OnResolutionSelected(int index) {
-		
-		GD.Print("option selected: " + index);
-		Vector2I selectedResolution = resolutions[index];
-		DisplayServer.WindowSetSize(selectedResolution);
+		resolutionManager.ChangeResolution(resolutions[index]);
+	}
+	
+	private void OnDisplayModeSelected(int index) {
+		resolutionManager.ChangeDisplayMode(index);
 	}
 
 	private void OnBackButtonPressed() {
