c5acbed69b4488982558b9b68a89e0cd124709b6
VenomiXeD <crafterious@gmail.com>
2025-04-05 12:43:45 +0200

    Initial setup for Unit testing framework
    Unit test example in `Testing/` folder

--- Scripts/Game/Model/Living/LivingAnimal.cs
+++ Scripts/Game/Model/Living/LivingAnimal.cs
@@ -1,36 +1,29 @@
 using System;
-using System.Collections.Generic;
 using Goodot15.Scripts.Game.Model;
 using Goodot15.Scripts.Game.Model.Interface;
 
 public class LivingAnimal(string textureAddress, bool movable, int cost, int health, CardNode cardNode)
-	: CardLiving(textureAddress, movable, cost, health, cardNode), IStackable, ITickable
-{
+	: CardLiving(textureAddress, movable, cost, health, cardNode), IStackable, ITickable {
 	private int produceTimer;
 
 	public IStackable NeighbourAbove { get; set; }
 	public IStackable NeighbourBelow { get; set; }
 
-	public void SetNeighbourAbove(IStackable card)
-	{
+	public void SetNeighbourAbove(IStackable card) {
 		throw new NotImplementedException();
 	}
 
-	public void SetNeighbourBelow(IStackable card)
-	{
+	public void SetNeighbourBelow(IStackable card) {
 		throw new NotImplementedException();
 	}
 
-	public int GetProduceTimer()
-	{
+	public void preTick() {
+	}
+
+	public void postTick() {
+	}
+
+	public int GetProduceTimer() {
 		return produceTimer--;
-	}
-
-	public void preTick()
-	{
-	}
-
-	public void postTick()
-	{
 	}
 }
---
+++ Tests/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using GdUnit4;
+using Godot;
+
+namespace Goodot15.Tests;
+
+[TestSuite]
+public class Test {
+	[TestCase]
+	// Very basic Unit test
+	public void Test_StackCard() {
+		// Load a scene
+		ISceneRunner runner = ISceneRunner.Load("res://Scenes/mainScene.tscn");
+		// Simulate that we pressed space
+		runner.SimulateKeyPress(Key.Space);
+		// Fetch if ANY CardNode has appeared on the screen - which it has
+		Assertions.AssertObject(runner.Scene().GetChildren().FirstOrDefault(e => e is CardNode)).IsNotNull();
+	}
+}
