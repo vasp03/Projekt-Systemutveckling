68f5a44e9208893cca8facad976924272c090d35
VenomiXeD <crafterious@gmail.com>
2025-04-05 14:01:45 +0200

    Add Play button test

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Threading.Tasks;
 using GdUnit4;
 using Godot;
 
@@ -6,14 +7,18 @@ namespace Goodot15.Tests;
 
 [TestSuite]
 public class Test {
+	
 	[TestCase]
 	// Very basic Unit test
-	public void Test_StackCard() {
+	public async Task Test_StackCard() {
 		// Load a scene
 		ISceneRunner runner = ISceneRunner.Load("res://Scenes/mainScene.tscn");
 		// Simulate that we pressed space
 		runner.SimulateKeyPress(Key.Space);
+		await ISceneRunner.SyncProcessFrame;
 		// Fetch if ANY CardNode has appeared on the screen - which it has
 		Assertions.AssertObject(runner.Scene().GetChildren().FirstOrDefault(e => e is CardNode)).IsNotNull();
 	}
+	
+
 }
