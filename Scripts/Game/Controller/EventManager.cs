767b5f0ae8950a571b60615f3795805fbfb5c2e4
kevintav <kevin@amanzi.com>
2025-04-12 16:29:20 +0200

    Created GameEvent.cs in Model/Div to represent game events. Added List of GameEvent.cs objects to EventManager.cs. Implemented preTick and postTick in EventManager.cs.

--- Scripts/Game/Controller/EventManager.cs
+++ Scripts/Game/Controller/EventManager.cs
@@ -1,5 +1,17 @@
-﻿namespace Goodot15.Scripts.Game.Controller;
+﻿using Goodot15.Scripts.Game.Model.Div;
+using Goodot15.Scripts.Game.Model.Interface;
+using Godot;
+using System;
+using System.Collections.Generic;
 
-public class EventManager {
-    
+namespace Goodot15.Scripts.Game.Controller;
+public partial class EventManager : Node, ITickable {
+    private List<GameEvent> events = new();
+    public void preTick() {
+        throw new NotImplementedException();
+    }
+
+    public void postTick() {
+        throw new NotImplementedException();
+    }
 }
---
+++ Scripts/Game/Model/Div/GameEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Goodot15.Scripts.Game.Model.Div;
+
+public class GameEvent {
+    public string  eventName;
+    public string  eventDescription;
+    public Action  eventAction;
+
+    public GameEvent(string eventName, string eventDescription, Action eventAction) {
+        this.eventName = eventName;
+        this.eventDescription = eventDescription;
+        this.eventAction = eventAction;
+    }
+}
