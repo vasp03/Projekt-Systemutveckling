d0a08e3b1842312fa85d5ecc95d6bfe3ec2907e9
kevintav <kevin@amanzi.com>
2025-04-12 16:37:12 +0200

    Removed event desc from GameEvent.cs, added PackedScene meteoriteCardScene and Node cardParent to EventManager.cs. Implemented _Ready method for loading meteoriteCardScene, the Node and adding MeteoriteStrike Event to list

--- Scripts/Game/Controller/EventManager.cs
+++ Scripts/Game/Controller/EventManager.cs
@@ -2,14 +2,24 @@
 using Goodot15.Scripts.Game.Model.Interface;
 using Godot;
 using System;
+
 using System.Collections.Generic;
+
 
 namespace Goodot15.Scripts.Game.Controller;
 public partial class EventManager : Node, ITickable {
     private List<GameEvent> events = new();
+    private PackedScene meteoriteCardScene;
+    private Node cardParent;
     private int tickCounter = 0;
     private int tickInterval = 60;
     private Random random = new();
+
+    public override void _Ready() {
+        meteoriteCardScene = GD.Load<PackedScene>("res://MeteoriteCard.tscn");
+        cardParent = GetNode("");
+        events.Add(new GameEvent("Meteorite Strike", SpawnMeteoriteStrike));
+    }
     public void preTick() {
         throw new NotImplementedException();
     }
--- Scripts/Game/Model/Div/GameEvent.cs
+++ Scripts/Game/Model/Div/GameEvent.cs
@@ -4,12 +4,10 @@ namespace Goodot15.Scripts.Game.Model.Div;
 
 public class GameEvent {
     public string  eventName;
-    public string  eventDescription;
     public Action  eventAction;
 
     public GameEvent(string eventName, string eventDescription, Action eventAction) {
         this.eventName = eventName;
-        this.eventDescription = eventDescription;
         this.eventAction = eventAction;
     }
 }
