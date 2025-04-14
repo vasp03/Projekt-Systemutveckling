9323677c54d3c4cbd3ef4c99a67aae4d2562b09a
Hampus Svärd <187299286+HampusSvard@users.noreply.github.com>
2025-04-14 16:40:40 +0200

    påbörjat cardtypes menyn i guide o lagt till continueknapp i mainmenu

--- Scripts/Game/View/GuideMenu.cs
+++ Scripts/Game/View/GuideMenu.cs
@@ -1,17 +1,77 @@
+using System.Collections.Generic;
 using Godot;
-using System;
 
 public partial class GuideMenu : Control {
-	private MenuController menuController;
-	
+	private VBoxContainer buildingList;
+	private Button buildingsButton;
+	private readonly Dictionary<Button, VBoxContainer> buttons = new();
+	private Label cardDescription;
+
+	private TextureRect cardImage;
+	private Button foodButton;
+	private VBoxContainer foodList;
+
 	private Button goBackButton;
+	private MenuController menuController;
+	private Button natureButton;
+	private VBoxContainer natureList;
+	private VBoxContainer resourceList;
+	private Button resourcesButton;
+
+	private VBoxContainer toolList;
+
+	private Button toolsButton;
+	private VBoxContainer villagerList;
+	private Button villagersButton;
 
 	public override void _Ready() {
 		menuController = GetNode<MenuController>("/root/MenuController");
+
+		toolsButton = GetNode<Button>("TabContainer/Card Types/CTBoxContainer/ToolsButton");
+		foodButton = GetNode<Button>("TabContainer/Card Types/CTBoxContainer/FoodButton");
+		buildingsButton = GetNode<Button>("TabContainer/Card Types/CTBoxContainer/BuildingsButton");
+		natureButton = GetNode<Button>("TabContainer/Card Types/CTBoxContainer/NatureButton");
+		villagersButton = GetNode<Button>("TabContainer/Card Types/CTBoxContainer/VillagersButton");
+		resourcesButton = GetNode<Button>("TabContainer/Card Types/CTBoxContainer/ResourcesButton");
+
+		toolList = GetNode<VBoxContainer>("TabContainer/Card Types/CTBoxContainer/ListPanel/ToolList");
+		foodList = GetNode<VBoxContainer>("TabContainer/Card Types/CTBoxContainer/ListPanel/FoodList");
+		buildingList = GetNode<VBoxContainer>("TabContainer/Card Types/CTBoxContainer/ListPanel/BuildingList");
+		natureList = GetNode<VBoxContainer>("TabContainer/Card Types/CTBoxContainer/ListPanel/NatureList");
+		villagerList = GetNode<VBoxContainer>("TabContainer/Card Types/CTBoxContainer/ListPanel/VillagerList");
+		resourceList = GetNode<VBoxContainer>("TabContainer/Card Types/CTBoxContainer/ListPanel/ResourceList");
+
+		buttons[toolsButton] = toolList;
+		buttons[foodButton] = foodList;
+		buttons[buildingsButton] = buildingList;
+		buttons[natureButton] = natureList;
+		buttons[villagersButton] = villagerList;
+		buttons[resourcesButton] = resourceList;
+
+		toolList.Visible = false;
+		foodList.Visible = false;
+		buildingList.Visible = false;
+		natureList.Visible = false;
+		villagerList.Visible = false;
+		resourceList.Visible = false;
+
+		toolsButton.Pressed += () => onButtonPressed(toolsButton);
+		foodButton.Pressed += () => onButtonPressed(foodButton);
+		buildingsButton.Pressed += () => onButtonPressed(buildingsButton);
+		natureButton.Pressed += () => onButtonPressed(natureButton);
+		villagersButton.Pressed += () => onButtonPressed(villagersButton);
+		resourcesButton.Pressed += () => onButtonPressed(resourcesButton);
+
 		goBackButton = GetNode<Button>("GoBackButton");
 		goBackButton.Pressed += OnGoBackButtonPressed;
 	}
-	
+
+
+	private void onButtonPressed(Button button) {
+		foreach (KeyValuePair<Button, VBoxContainer> choice in buttons)
+			choice.Value.Visible = choice.Key == button && !choice.Value.Visible;
+	}
+
 	private void OnGoBackButtonPressed() {
 		menuController.GoBackToPreviousMenu();
 	}
--- Scripts/Game/View/MainMenu.cs
+++ Scripts/Game/View/MainMenu.cs
@@ -5,29 +5,44 @@ public partial class MainMenu : Control {
 	
 	private SoundController soundController;
 	private MenuController menuController;
+	private Button continueButton;
 	private Button optionsButton;
 	private Button playButton;
 	private Button guideButton;
 	private Button exitButton;
-	
+	private bool canContinue = false;
+
 	public override void _Ready() {
 		menuController = GetNode<MenuController>("/root/MenuController");
 		menuController.configureWithNewMainMenuInstance(this);
 		soundController = GetNode<SoundController>("/root/SoundController");
 		soundController.PlayMenuMusic();
-		
+
+		continueButton = GetNode<Button>("ButtonContainer/ContinueButton");
+		continueButton.Pressed += OnContinueButtonPressed;
 
 		playButton = GetNode<Button>("ButtonContainer/PlayButton");
 		playButton.Pressed += OnPlayButtonPressed;
 
 		optionsButton = GetNode<Button>("ButtonContainer/OptionsButton");
 		optionsButton.Pressed += OnOptionsButtonPressed;
-		
+
 		guideButton = GetNode<Button>("ButtonContainer/GuideButton");
 		guideButton.Pressed += OnGuideButtonPressed;
 
 		exitButton = GetNode<Button>("ButtonContainer/ExitButton");
 		exitButton.Pressed += OnExitButtonPressed;
+
+		if (!canContinue) {
+			continueButton.Disabled = true;
+		}
+		else {
+			continueButton.Disabled = false;
+		}
+	}
+
+	private void OnContinueButtonPressed() {
+		//continue saved game
 	}
 
 	private void OnPlayButtonPressed() {
