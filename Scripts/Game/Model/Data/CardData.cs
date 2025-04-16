40c9604b7a85e41ef7ff00fb824a08e869eb33b6
VenomiXeD <crafterious@gmail.com>
2025-04-16 15:40:54 +0200

    Initial framework(?)

---
+++ Scripts/Game/Model/Data/CardData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Goodot15.Scripts.Game.Model.Data;
+
+public class CardData {
+    public string CardTypeName { get; set; }
+    public Type CardTypeClass => Type.GetType(this.CardTypeName);
+    
+    public object CardTypeData { get; set; }
+}
---
+++ Scripts/Game/Model/Data/SaveGame.cs
@@ -0,0 +1,7 @@
+﻿namespace Goodot15.Scripts.Game.Model.Data;
+
+public class SaveGame {
+    public double PlayerEconomy { get; set; }
+    // TODO: Saving cards data?
+    public CardData[] Cards { get; set; }
+}
