6a96e3874f46e03a65a2d329da85e4bc7be062d6
kactuz <viktorasp23@gmail.com>
2025-04-24 11:17:20 +0200

    Update CardCreationHelper.cs

--- Scripts/Game/Controller/CardCreationHelper.cs
+++ Scripts/Game/Controller/CardCreationHelper.cs
@@ -79,7 +79,8 @@ public class CardCreationHelper {
                 return new MaterialGlass();
             case "Leaves" or "19":
                 return new MaterialLeaves();
-
+            case "Clay" or "20":
+                return new MaterialClay();
 
             case "SwordMK1" or "21":
                 return new MaterialSwordMk1();
