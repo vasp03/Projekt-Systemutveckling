eb792c2d0bef2d37469e292703b99c46707aefa8
kevintav <kevin@amanzi.com>
2025-05-01 16:29:48 +0200

    Background fix
    Removed old background png and added new updated name

--- Scripts/Game/Controller/CraftButton.cs
+++ Scripts/Game/Controller/CraftButton.cs
@@ -2,18 +2,18 @@ using Godot;
 using System;
 
 public partial class CraftButton : Node2D {
-    public CardController CardController { get; set; }
+	public CardController CardController { get; set; }
 
-    public string NameOfButton { get; set; }
+	public string NameOfButton { get; set; }
 
-    public CardNode CardNode { get; set; }
+	public CardNode CardNode { get; set; }
 
-    public void _on_button_button_down() {
-    }
+	public void _on_button_button_down() {
+	}
 
-    public void _on_button_button_up() {
-        // Handle the button release event
-        GD.Print("Craft button released.");
-        CardController.CraftCardFromSpecifiedCardNode(CardNode);
-    }
+	public void _on_button_button_up() {
+		// Handle the button release event
+		GD.Print("Craft button released.");
+		CardController.CraftCardFromSpecifiedCardNode(CardNode);
+	}
 }
