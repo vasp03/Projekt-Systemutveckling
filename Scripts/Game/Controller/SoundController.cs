fc2f7ab86d6dd97bc38243df0750ad10c275f120
VenomiXeD <crafterious@gmail.com>
2025-05-14 19:12:30 +0200

    XML Doc for SoundController.cs

--- Scripts/Game/Controller/SoundController.cs
+++ Scripts/Game/Controller/SoundController.cs
@@ -13,16 +13,16 @@ public partial class SoundController : Node {
     private float _sfxVolume;
     private string CurrentPlayingMusicPath;
     private AudioStreamPlayer MusicPlayer;
+    /// <summary>
+    /// Settings manager instance reference
+    /// </summary>
     private SettingsManager SettingsManager => GetNode<SettingsManager>("/root/SettingsManager");
+    
+    /// <summary>
+    /// Determines if sound effects are muted
+    /// </summary>
     public bool SfxMuted { get; set; }
 
-    public bool MusicMuted {
-        get => _musicMuted;
-        set {
-            _musicMuted = value;
-            UpdateMusicMuted();
-        }
-    }
 
     public float SfxVolume {
         get => _sfxVolume;
@@ -71,21 +71,26 @@ public partial class SoundController : Node {
         // MusicPlayer.Finished += OnMusicFinished;
     }
 
-    /*
-     * Helper method for OnMusicFinished class
-     * Checks to see if a song should replay after .finished has emitted
-     * Necessary cause MP3 songs don't have built in looping through Godot
-     */
-    public bool ShouldCurrentSongLoop() {
-        if (string.IsNullOrEmpty(CurrentPlayingMusicPath)) return false;
-
-        return CurrentPlayingMusicPath.Contains("DayTimeSongs/Day");
+    /// <summary>
+    ///     Helper method for OnMusicFinished class
+    ///     Checks to see if a song should replay after .finished has emitted
+    ///     Necessary cause MP3 songs don't have built in looping through Godot
+    /// </summary>
+    /// <returns></returns>
+    private bool ShouldCurrentSongLoop() {
+        return !string.IsNullOrEmpty(CurrentPlayingMusicPath) && CurrentPlayingMusicPath.Contains("DayTimeSongs/Day");
     }
 
+    /// <summary>
+    /// Plays regular (main) menu music
+    /// </summary>
     public void PlayMenuMusic() {
         PlayMusic("xDeviruchi/02 - Title Theme.wav");
     }
 
+    /// <summary>
+    /// Plays regular game music
+    /// </summary>
     public void PlayGameMusic() {
         PlayMusic("xDeviruchi/03 - Definitely Our Town.wav");
     }
@@ -94,6 +99,10 @@ public partial class SoundController : Node {
         PlayMusic("xDeviruchi/08 - Shop.wav");
     }
 
+    /// <summary>
+    /// Plays a song with for the specified <see cref="dayTime"/> DayTime value. Automatically loads and caches the sound asset after
+    /// </summary>
+    /// <param name="dayTime">Relevant Day-time</param>
     public void PlayDayTimeSong(string dayTime) {
         string musicPath = $"DayTimeSongs/{dayTime}.mp3";
 
@@ -105,6 +114,10 @@ public partial class SoundController : Node {
         PlayMusic(musicPath);
     }
 
+    /// <summary>
+    /// Plays the specified <see cref="musicPath"/> music.
+    /// </summary>
+    /// <param name="musicPath">Music path, only the music path is needed</param>
     private void PlayMusic(string musicPath) {
         if (CurrentPlayingMusicPath == musicPath) {
             MusicPlayer.Play();
@@ -119,6 +132,11 @@ public partial class SoundController : Node {
         MusicPlayer.Play();
     }
 
+    /// <summary>
+    /// Loads the specified music sound asset and caches it in memory
+    /// </summary>
+    /// <param name="soundAssetName">Music asset to be loaded</param>
+    /// <returns>Loaded music as an AudioStream</returns>
     private AudioStream LoadMusic(string soundAssetName) {
         if (CachedMusic.TryGetValue(soundAssetName, out AudioStream audioStream)) return audioStream;
 
@@ -130,6 +148,9 @@ public partial class SoundController : Node {
         return audioStream;
     }
 
+    /// <summary>
+    /// Stops playing the current music
+    /// </summary>
     public void StopMusic() {
         MusicPlayer?.Stop();
         CurrentPlayingMusicPath = "";
@@ -137,6 +158,10 @@ public partial class SoundController : Node {
 
     private const string BASE_SOUND_PATH = "res://Assets/Sounds";
 
+    /// <summary>
+    /// Plays a sound effect with for the specified <see cref="dayTime"/> DayTime value. Automatically loads and caches the sound asset after
+    /// </summary>
+    /// <param name="dayTime">Relevant Day-time</param>
     public void PlaySound(string soundName) {
         AudioStreamPlayer player = new();
         player.Stream = LoadSound(soundName);
@@ -149,6 +174,11 @@ public partial class SoundController : Node {
         player.Play();
     }
 
+    /// <summary>
+    /// Loads the specified sound effect asset and caches it in memory
+    /// </summary>
+    /// <param name="soundAssetName">Music asset to be loaded</param>
+    /// <returns>Loaded sound effect as an AudioStream</returns>
     private AudioStream LoadSound(string soundAssetName) {
         if (CachedMusic.TryGetValue(soundAssetName, out AudioStream audioStream))
             // Return already loaded asset
@@ -166,6 +196,20 @@ public partial class SoundController : Node {
 
     #region Settings & configurability related
 
+    /// <summary>
+    /// Determines if music is muted or not
+    /// </summary>
+    public bool MusicMuted {
+        get => _musicMuted;
+        set {
+            _musicMuted = value;
+            UpdateMusicMuted();
+        }
+    }
+    
+    /// <summary>
+    /// The current music volume, range is <b>0-1</b> inclusive.
+    /// </summary>
     public float MusicVolume {
         get => _musicVolume;
         set {
@@ -174,25 +218,39 @@ public partial class SoundController : Node {
         }
     }
 
+    /// <summary>
+    /// Fired when music volume changes, applies the new music volume to the music player
+    /// </summary>
     private void UpdateMusicVolume() {
         if (!MusicMuted) MusicPlayer.VolumeDb = Mathf.LinearToDb(MusicVolume);
     }
-
+    
+    /// <summary>
+    /// Toggles if music is muted or not
+    /// </summary>
+    /// <returns>True or false if music was just muted or not</returns>
     public bool ToggleMusicMuted() {
         MusicMuted = !MusicMuted;
         UpdateMusicMuted();
         return MusicMuted;
     }
 
+    /// <summary>
+    /// Fired when music is muted changes, applies the new muted property to the music player
+    /// </summary>
     private void UpdateMusicMuted() {
         MusicPlayer.VolumeDb = MusicMuted
             ? -80
             : Mathf.LinearToDb(MusicVolume);
     }
 
+    /// <summary>
+    /// Toggles if sound effects are muted or not
+    /// </summary>
+    /// <returns>True or false if sound effects were just muted or not</returns>
     public bool ToggleSfxMuted() {
         return SfxMuted = !SfxMuted;
     }
-
+    
     #endregion Settings & configurability related
 }
