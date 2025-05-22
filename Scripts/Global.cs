502490daf6b7be076127b2f11b64757ec2e7d0d7
kactuz <viktorasp23@gmail.com>
2025-05-22 12:58:00 +0200

    Mouse fix

--- Scripts/Global.cs
+++ Scripts/Global.cs
@@ -1,24 +1,40 @@
+using System;
 using Godot;
+using Goodot15.Scripts.Game.Controller;
 
-namespace Goodot15.Scripts;
+namespace Goodot15.Scripts.Game;
 
 public partial class Global : Node {
-    public delegate void MoneyChangedEventHandler(int newMoney);
+    private int money;
+    public static Global Singleton { get; private set; }
 
-    public int Money { get; set; }
-
-    public void AddMoney(int newMoney) {
-        Money += newMoney;
-        EmitSignal("MoneyChanged", newMoney);
-    }
-
-    public bool SpendMoney(int newMoney) {
-        if (Money >= newMoney) {
-            Money -= newMoney;
-            EmitSignal("MoneyChanged", newMoney);
-            return true;
+    private static MouseController mouseController = new MouseController();
+    public static MouseController MouseController {
+        get {
+            if (mouseController == null) mouseController = new MouseController();
+            return mouseController;
         }
-
-        return false;
+        private set {
+            return;
+        }
     }
+
+    public int Money {
+        get => money;
+        private set {
+            money = value;
+            MoneyChanged?.Invoke(money);
+        }
+    }
+
+    public override void _Ready() {
+        Singleton = this;
+    }
+
+    public void AddMoney(int amount) {
+        Money += amount;
+        GD.Print($"[Global] Money updated: {Money}");
+    }
+
+    public event Action<int> MoneyChanged;
 }
