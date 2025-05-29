5a4e7c2d476a0766f83f6a502ec11083948a4d85
Viktor Asp <viktorasp23@gmail.com>
2025-05-29 09:28:52 +0200

    Alter changes the texture based on nr. of sacrified

--- Scripts/Game/Model/Parents/Altar.cs
+++ Scripts/Game/Model/Parents/Altar.cs
@@ -1,26 +1,46 @@
 
 
+using Godot;
 using Goodot15.Scripts.Game.Controller;
 using Goodot15.Scripts.Game.Model.Interface;
 using Goodot15.Scripts.Game.Model.Living;
 
 namespace Goodot15.Scripts.Game.Model.Parents;
 
-public class Altar() : Card("Altar", true), ICardConsumer, ITickable {
-    private const int TOTAL_SACRIFICES_REQUIRED = 3;
-    private int totalSacrificesSacrified = 0;
+public class Altar() : Card("Alter_0", true), ICardConsumer, ITickable {
+    private const int TOTAL_SACRIFICES_REQUIRED = 10;
+    private int totalVillagersSacrificed = 0;
     private bool playedSound = false;
     public override int Value => -1;
 
+    private Texture2D stage0 = GD.Load<Texture2D>("res://Assets/Cards/Ready To Use/Alter_0.png");
+    private Texture2D stage1 = GD.Load<Texture2D>("res://Assets/Cards/Ready To Use/Alter_1.png");
+    private Texture2D stage2 = GD.Load<Texture2D>("res://Assets/Cards/Ready To Use/Alter_2.png");
+    private Texture2D stage3 = GD.Load<Texture2D>("res://Assets/Cards/Ready To Use/Alter_3.png");
+    private Texture2D stage4 = GD.Load<Texture2D>("res://Assets/Cards/Ready To Use/Alter_4.png");
+
     public bool ConsumeCard(Card otherCard) {
-        if (otherCard is not LivingPlayer && totalSacrificesSacrified < TOTAL_SACRIFICES_REQUIRED) return false;
-        totalSacrificesSacrified++;
-        GameController.Singleton.CameraController.Shake(totalSacrificesSacrified, Utilities.TimeToTicks(seconds: 0.75d));
+        if (otherCard is not LivingPlayer || totalVillagersSacrificed > TOTAL_SACRIFICES_REQUIRED) return false;
+        totalVillagersSacrificed++;
+        GameController.Singleton.CameraController.Shake(totalVillagersSacrificed, Utilities.TimeToTicks(seconds: 0.75d));
+
+        if (totalVillagersSacrificed == 0) {
+            CardNode.UpdateCardTexture(stage0, true);
+        } else if (totalVillagersSacrificed >= 1 && totalVillagersSacrificed <= TOTAL_SACRIFICES_REQUIRED * 0.33) {
+            CardNode.UpdateCardTexture(stage1, true);
+        } else if (totalVillagersSacrificed > TOTAL_SACRIFICES_REQUIRED * 0.25 && totalVillagersSacrificed <= TOTAL_SACRIFICES_REQUIRED * 0.67) {
+            CardNode.UpdateCardTexture(stage2, true);
+        } else if (totalVillagersSacrificed > TOTAL_SACRIFICES_REQUIRED * 0.5 && totalVillagersSacrificed < TOTAL_SACRIFICES_REQUIRED) {
+            CardNode.UpdateCardTexture(stage3, true);
+        } else if (totalVillagersSacrificed >= TOTAL_SACRIFICES_REQUIRED) {
+            CardNode.UpdateCardTexture(stage4, true);
+        }
+
         return true;
     }
 
     public void PostTick(double delta) {
-        if (totalSacrificesSacrified >= TOTAL_SACRIFICES_REQUIRED && !playedSound) {
+        if (totalVillagersSacrificed >= TOTAL_SACRIFICES_REQUIRED && !playedSound) {
             GameController.Singleton.SoundController.PlaySound("General Sounds/Interactions/sfx_sounds_interaction5.wav");
             playedSound = true;
             GameController.Singleton.CameraController.PlayEndGameAnimation();
