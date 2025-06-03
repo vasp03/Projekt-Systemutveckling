f1fae5aa1ac3a464b7f2eb58e8c46d2bb236375c
VenomiXeD <crafterious@gmail.com>
2025-06-03 11:49:57 +0200

    Cleanup by Rider

--- Scripts/Game/Controller/MenuController.cs
+++ Scripts/Game/Controller/MenuController.cs
@@ -14,21 +14,21 @@ public partial class MenuController : Node {
     public static MenuController Singleton =>
         (Engine.GetMainLoop() as SceneTree).CurrentScene.GetNode<MenuController>("/root/MenuController");
 
+    /// <summary>
+    ///     Checks if the game over menu is currently active and visible.
+    /// </summary>
+    /// <returns>true if game over is active. False if game over isn't active</returns>
+    public bool IsGameOverMenuActive =>
+        gameOverMenu is not null && gameOverMenu.IsInsideTree() && gameOverMenu.Visible;
+
     public override void _Ready() {
         currentMenu = mainMenu;
         pauseMenu = null;
         optionsMenu = null;
         guideMenu = null;
     }
-    
-    /// <summary>
-    /// Checks if the game over menu is currently active and visible.
-    /// </summary>
-    /// <returns>true if game over is active. False if game over isn't active</returns>
-    public bool IsGameOverMenuActive => 
-        gameOverMenu is not null && gameOverMenu.IsInsideTree() && gameOverMenu.Visible;
 
-    
+
     /// <summary>
     ///     Configures the MenuController with a new instance of the MainMenu.
     /// </summary>
--- Scripts/Utilities.cs
+++ Scripts/Utilities.cs
@@ -1,9 +1,8 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Godot;
 using Goodot15.Scripts.Game.Model.Enums;
 
-namespace Goodot15.Scripts;
-
-public static class Utilities {
+s Utilities {
     public const double TICKS_PER_SECOND = 60;
 
     /// <summary>
